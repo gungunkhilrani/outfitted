c75dc6d5682c0620cf1d8edaf6f9013f56cc4977
gungunkhilrani <gungunkhilrani446@gmail.com>
2024-07-10 20:31:31 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images:{
         domains:['lh3.googleusercontent.com','firebasestorage.googleapis.com']
     }
+    output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+  basePath: "/nextjs-github-pages",
+
 };
 
 export default nextConfig;
