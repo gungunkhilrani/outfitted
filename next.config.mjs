f3299b96ef7f7e7a51806db0868f50f12f021353
gungunkhilrani <gungunkhilrani446@gmail.com>
2024-07-11 09:03:41 +0530

    initial commit of Outfitted project

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
     images:{
         domains:['lh3.googleusercontent.com','firebasestorage.googleapis.com']
     }
-    output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-  basePath: "/nextjs-github-pages",
 
 };
 
